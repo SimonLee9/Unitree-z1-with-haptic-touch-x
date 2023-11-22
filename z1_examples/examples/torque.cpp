6150de996ecc40eb2d75aefa0ac53858782a3b3c
Beomjin Lee <74641464+SimonLee9@users.noreply.github.com>
2023-11-22 22:02:53 +0900

    Add files via upload

---
+++ z1_examples/examples/torque.cpp
@@ -0,0 +1,100 @@
+#include <moveit/move_group_interface/move_group_interface.h>
+
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+
+#include <moveit_visual_tools/moveit_visual_tools.h>
+
+
+//#include <moveit/motion_planning_rviz_plugin/motion_planning_display.h>
+//#include <moveit/motion_planning_rviz_plugin/motion_planning_frame_joints_widget.h>
+
+geometry_msgs::Pose haptic_pose_msg;
+
+void poseCallback(const geometry_msgs::Pose::ConstPtr &msg) {
+    haptic_pose_msg.orientation.x = msg->orientation.x;
+    haptic_pose_msg.orientation.y = msg->orientation.y;
+    haptic_pose_msg.orientation.z = msg->orientation.z;
+    haptic_pose_msg.orientation.w = msg->orientation.w;
+    haptic_pose_msg.position.x = msg->position.x;
+    haptic_pose_msg.position.y = msg->position.y;
+    haptic_pose_msg.position.z = msg->position.z;
+
+    haptic_pose_msg.position = msg->position;
+}
+
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "move_group_interface");
+  ros::NodeHandle nh;
+
+  ros::AsyncSpinner spinner(0);
+  spinner.start();
+
+  static const std::string PLANNING_GROUP = "manipulator";
+
+  moveit::planning_interface::MoveGroupInterface move_group_interface(PLANNING_GROUP);
+  move_group_interface.setMaxVelocityScalingFactor(0.15);
+  move_group_interface.setMaxAccelerationScalingFactor(0.5);
+
+  const moveit::core::JointModelGroup* jmg = move_group_interface.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+  ros::Subscriber pose_sub = nh.subscribe("haptic_pose", 1,::poseCallback);
+
+  /* Move to init position */
+  
+  geometry_msgs::Pose target_pose1;
+  /*
+  target_pose1.orientation.w = 0;
+  target_pose1.position.x = 0.3; // origin : 0.3
+  target_pose1.position.y = 0; // origin : 0
+  target_pose1.position.z = 0.4; // origin : 0.4
+  */
+
+  //move_group_interface.setPoseTarget(target_pose1);
+  move_group_interface.setPoseTarget(haptic_pose_msg);
+
+  moveit::planning_interface::MoveGroupInterface::Plan plan1;
+  bool success = (move_group_interface.plan(plan1) == moveit::core::MoveItErrorCode::SUCCESS);
+
+  if(success) {
+    move_group_interface.execute(plan1);
+  }
+
+  // Cartesinan Paths
+  // ^^^^^^^^^^^^^^^^
+  std::vector<geometry_msgs::Pose> waypoints;
+  //waypoints.push_back(target_pose1);
+  waypoints.push_back(haptic_pose_msg);
+
+
+  //geometry_msgs::Pose target_pose2 = target_pose1;
+  geometry_msgs::Pose target_pose2 = haptic_pose_msg;
+
+  target_pose2.position.x += 0.1;
+  waypoints.push_back(target_pose2); // forward
+
+  target_pose2.position.z -= 0.1;
+  waypoints.push_back(target_pose2); // up
+
+  moveit_msgs::RobotTrajectory trajectory;
+  const double jump_threhold = 0.0;
+  const double eef_step = 0.01;
+  double fraction = move_group_interface.computeCartesianPath(waypoints, eef_step, jump_threhold, trajectory);
+
+  moveit::planning_interface::MoveGroupInterface::Plan plan2;
+  plan2.trajectory_ = trajectory;
+  move_group_interface.execute(plan2);
+  
+
+  ros::waitForShutdown();
+  //ros::shutdown();
+  //ros::spin();
+  
+  return 0;
+}
---
+++ z1_examples/scripts/get_torque.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import time
+import rospkg
+z1_sdk_path = rospkg.RosPack().get_path('z1_sdk')
+import sys
+sys.path.append(z1_sdk_path+"/lib")
+import unitree_arm_interface
+import numpy as np
+import rospy
+
+from std_msgs.msg import Float64
+
+'''
+example_low_cmd.py
+An example showing how to control arm motors directly.
+
+Run `roslaunch z1_bringup **_ctrl.launch` first.
+'''
+
+# Connect to z1_controller
+z1 =  unitree_arm_interface.UnitreeArm("127.0.0.1", 8872)
+z1.init()
+
+if z1.armState.mode != int(unitree_arm_interface.ArmMode.Passive):
+  print("ERROR] Please repower the arm to use lowcmd mode.")
+  sys.exit(-1)
+
+
+rospy.init_node('tau_publisher', anonymous=True)
+#tau_publisher = [rospy.Publisher('joint_tau_{i}', Float64, queue_size=10) for i in range(6)]
+tau_publisher = rospy.Publisher('joint_tau', Float64, queue_size=10)
+
+while not rospy.is_shutdown():
+  for i in range (6):
+    tau_msg = Float64()
+    tau_msg.data=z1.armState.tau[i]
+    tau_publisher.publish(tau_msg) # unlock
+    #tau_publisher.publish(z1.armState.tau[0])
+    #tau_publisher[i].publish(tau_msg)
+
+  time.sleep(0.1)
+  #tau_publisher.publish(z1.armState.tau[0]) # unlock
+  #time.sleep(z1.dt) # unlock
+
+'''
+# Change to State_lowcmd mode
+z1.armCmd.mode = int(unitree_arm_interface.ArmMode.LowCmd)
+z1.armCmd.q_d = z1.armState.q
+z1.armCmd.gripperCmd.angle = z1.armState.gripperCmd.angle
+z1.armCmd.dq_d = np.zeros(6)
+z1.armCmd.tau_d = np.zeros(6)
+
+z1.armCmd.Kp = [500, 600, 500, 400, 300, 200]
+z1.armCmd.Kd = [5, 5, 5, 5, 5, 5]
+z1.sendRecv()
+
+jnt_speed = 0.2
+
+q_d = z1.armState.q_d
+
+
+#for i in range(0, 300):
+while True:
+  #q_d[0] += jnt_speed * z1.dt
+  #z1.armCmd.q_d = q_d
+  print('Joiint Torque') # Test
+  print('joint1:',z1.armState.tau[0]) # Test
+  print('joint2:',z1.armState.tau[1]) # Test
+  print('joint3:',z1.armState.tau[2]) # Test
+  print('joint4:',z1.armState.tau[3]) # Test
+  print('joint5:',z1.armState.tau[4]) # Test
+  print('joint6:',z1.armState.tau[5]) # Test
+
+
+  z1.armCmd.tau_d = np.zeros(6)
+  z1.sendRecv()
+  time.sleep(z1.dt)
+'''
+
+z1.armCmd.mode = int(unitree_arm_interface.ArmMode.Passive)
+z1.sendRecv()
+
+
+
